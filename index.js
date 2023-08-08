239daf833aa697ff98827cef3f87dbc60f4e216d
JFernandez0524 <njphonefix@gmail.com>
2023-08-08 09:09:55 -0400

    added morgan cors bodyparser

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 require('dotenv').config();
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+const envelopes = [];
+const totalBudget = 0;
+
+app.use(morgan('tiny'));
+app.use(cors);
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/envelopes', (req, res) => {
+  res.send('Here are the envelopes.s');
+});
+
+app.post('/envelopes', (req, res) => {
+  res.send('Post Request');
 });
 
 app.listen(port, () => {
